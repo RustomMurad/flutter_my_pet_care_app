03ad4a33724ba684db92b28cab455bb7f3b67e45
Roman M Lagunov <roman-m-lagunov@MacBook-Pro-4.local>
2024-07-04 20:16:31 -0700

    Changes to be committed: *       modified with constructor params:   lib/models/pet.dart * 	add dev dependence package - uuid.dart

--- lib/models/pet.dart
+++ lib/models/pet.dart
@@ -1,6 +1,22 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+enum Gender { she, he }
+
 class Pet {
-  // final String id;
-  // final String name;
-  // final String breed;
-  // final Date dob;
+  Pet({
+    required this.name,
+    required this.breed,
+    required this.dob,
+    required this.sex,
+    required this.weight,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String name;
+  final String breed;
+  final DateTime dob;
+  final Gender sex;
+  final double weight;
 }
