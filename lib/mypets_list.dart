691ca9084a7ecfe6bf1f9c56725ceee838183865
Roman M Lagunov <roman-m-lagunov@MacBook-Pro-4.local>
2024-07-15 23:56:50 -0700

    Changes to be committed:        new file:   lib/models/mypet_avatar_item.dart        modified:   lib/mypets_list.dart

--- lib/mypets_list.dart
+++ lib/mypets_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_pets_care_app/models/mypet.dart';
+import 'package:my_pets_care_app/models/mypet_avatar_item.dart';
 import 'package:my_pets_care_app/mypet_item.dart';
 
 class MyPetsList extends StatelessWidget {
@@ -14,8 +15,12 @@ class MyPetsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: mypets.length,
-      itemBuilder: (ctx, index) => Dismissible(key: ValueKey(mypets[index]),
-      child: MypetItem(mypets[index])),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(mypets[index]),
+        child: MypetAvatarItem(
+          mypets[index],
+        ),
+      ),
     );
   }
 }
